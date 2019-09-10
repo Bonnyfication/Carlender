7a770eeb605a655429d6c73839fc3b46ceafc54a
Bonnyfication <34171562+Bonnyfication@users.noreply.github.com>
2019-09-10 20:35:36 +0200

    - DateTime

--- NetCarlender/App_Plugins/showSection/backoffice/showTree/edit.Controller.js
+++ NetCarlender/App_Plugins/showSection/backoffice/showTree/edit.Controller.js
@@ -8,22 +8,62 @@
         $scope.loaded = true;
     }
     else {
-        // Lade Artikeldaten
+        // Lade Carshow Daten
         showResource.getById($routeParams.id).then(function (response) {
             $scope.show = response.data;
-            $scope.dateValue = $scope.show.Datum;
-            $scope.datepicker.value = $scope.dateValue;
+
+            //$scope.datepicker.value
+            //$scope.StartTime.value = $scope.show.Datum;
+
+
+            $scope.StartTime =  {
+               
+                model: {
+                    editor: "Umbraco.DateTime",
+                    label: 'Start Time',
+                    description: 'Enter the date/time the meeting will start.',
+                    hideLabel: false,
+                    view: "datepicker",
+                    alias: 'StartTime',
+                    value: $scope.show.Datum,
+                    required: true,
+                    validation: {
+                        mandatory: true,
+                        pattern: ""
+                    },
+                    config: {
+                        format: "YYYY-MM-DD HH:mm:ss",
+                        pickDate: true,
+                        pickTime: true,
+                        useSeconds: false
+                    }
+                }
+                
+            };
+
+
+
+
+
+            $scope.richEditor.value = $scope.show.Beschreibung;
 
             $scope.loaded = true;
         });
     }
 
+    // SAVE ACTION
     $scope.save = function (show) {
+        // GET DATUM
         $scope.show.Datum = $scope.datepicker.value;
+        // GET BESCHREIBUNG
+        $scope.show.Beschreibung = $scope.richEditor.value;
 
         showResource.save(show).then(function (response) {
             $scope.show = response.data;
             notificationsService.success("Success", show.Titel + " " + " has been saved.");
+
+            // Confirm object isnt dirty anymore
+            $scope.carshowForm.$dirty = false;
 
             navigationService.syncTree({ tree: 'showTree', path: [-1, $scope.id], forceReload: true }).then(function
                 (syncArgs) {
@@ -32,26 +72,63 @@
         });
     }
 
-    $scope.datepicker = {
-        view: 'datepicker',
-        config: {
-            pickDate: true,
-            pickTime: true,
-            pick12HourFormat: false,
-            format: "DD/MM/YYYY HH:mm"
-        },
-        value: $scope.dateValue
+    // DATEPICKER
+
+    //$scope.datepicker = {
+    //    editor: "Umbraco.DateTime",
+    //    label: 'datepicker',
+    //    description: 'carshow datepicker',
+    //    hideLabel: false,
+    //    view: 'datepicker',
+    //    alias: 'datepicker',
+    //    value: null,
+    //    config: {
+    //        pickDate: true,
+    //        pickTime: true,
+    //        pick12HourFormat: false,
+    //        format: "YYYY-MM-DD HH:mm:ss",
+    //        icons: {
+    //            time: "icon-time",
+    //            date: "icon-calendar",
+    //            up: "icon-chevron-up",
+    //            down: "icon-chevron-down"
+    //        }
+    //    }
+    //};
+
+    $scope.StartTime = {
+        model: {
+            editor: "Umbraco.DateTime",
+            label: 'Start Time',
+            description: 'Enter the date/time the meeting will start.',
+            hideLabel: false,
+            view: "datepicker",
+            alias: 'StartTime',
+            value: "",
+            required: true,
+            validation: {
+                mandatory: true,
+                pattern: ""
+            },
+            config: {
+                format: "YYYY-MM-DD HH:mm",
+                pickDate: true,
+                pickTime: true,
+                useSeconds: false
+            }
+        }
     };
 
 
+    // MEDIAPICKER
     $scope.openMediaPicker = function () {
         $scope.mediaPickerOverlay = {
             view: "mediapicker",
             title: "Select Picture",
             startNodeId: 0,
-            multiPicker: false, // adjust as per your requirement
-            onlyImages: true, // adjust as per your requirement
-            disableFolderSelect: true, // adjust as per your requirement
+            multiPicker: false, 
+            onlyImages: true, 
+            disableFolderSelect: true,
             show: true,
             submit: function (model) {
 
@@ -63,5 +140,20 @@
             }
         };
     }
+
+    // RICH EDITOR
+    $scope.richEditor = {
+        label: 'Carshow Description Editor',
+        description: 'Carshow Description',
+        view: 'rte',
+        value: '',
+        config: {
+            editor: {
+                toolbar: ["code", "undo", "redo", "cut", "styleselect", "bold", "italic", "alignleft", "aligncenter", "alignright", "bullist", "numlist", "link", "umbmediapicker", "umbmacro", "umbembeddialog"],
+                stylesheets: [],
+                dimensions: {}
+            }
+        }
+    };
 
 });
